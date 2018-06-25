5e00993e6434961a03d28dc91f02a4802deb7299
Sergey Mishin <sergei.a.mishin@gmail.com>
2018-06-25 23:53:03 +0300

    Add CSV parsing utils and primitives
    This commit includes several changes:
    
    - Add the csv representation as a slice of the slices if the strings
      (see rawcsv module)
    
    - Add csv reader with column filtering (see csvreader module)
    
    - Add a summary type to summarize the values in columns by rows
      (see summary module)
    
    Resolves #38

---
+++ pkg/csvparse/rawcsv/rawcsv.go
@@ -0,0 +1,139 @@
+package rawcsv
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+
+	"github.com/pavlov-tony/xproject/pkg/csvparse/csvparseutils"
+	"github.com/pavlov-tony/xproject/pkg/csvparse/errors"
+	"github.com/pavlov-tony/xproject/pkg/csvparse/summary"
+)
+
+// RawCsv represents the matrix of strings parsed from CSV
+type RawCsv struct {
+	records [][]string
+}
+
+// FromRows creates RawCsv from [][]string
+func RawCsvFromRows(rows [][]string) *RawCsv {
+	return &RawCsv{
+		records: rows,
+	}
+}
+
+// FromReader creates RawCsv from io.Reader
+func RawCsvFromReader(in io.Reader) (*RawCsv, error) {
+	r := csv.NewReader(in)
+	records, err := r.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("can't create RawCsv from io.Reader: %v", err)
+	}
+
+	return &RawCsv{
+		records: records,
+	}, nil
+}
+
+// Rows returns the containing rows of RawCsv
+func (raw *RawCsv) Rows() [][]string {
+	return raw.records
+}
+
+// Row returns the row of RawCsv by index
+func (raw *RawCsv) Row(index int) ([]string, error) {
+	if index < len(raw.records) {
+		return raw.records[index], nil
+	} else {
+		return nil, errors.NewIndexError(index)
+	}
+}
+
+// ColumnIndexByName returns the number of the provided header name
+func (raw *RawCsv) ColumnIndexByName(name string) (int, error) {
+	if len(raw.records) == 0 {
+		return 0, fmt.Errorf("RawCsv is empty.")
+	}
+
+	header := raw.records[0]
+
+	for i, colName := range header {
+		if colName == name {
+			return i, nil
+		}
+	}
+
+	return 0, fmt.Errorf("header with name \"%v\" was not found", name)
+}
+
+// FilterByNames returns RawCsv with provided header names only
+func (raw *RawCsv) FilterByNames(columns []string) (*RawCsv, error) {
+	indices := make([]int, len(columns))
+	for i, columnName := range columns {
+		idx, err := raw.ColumnIndexByName(columnName)
+		if err != nil {
+			return nil, fmt.Errorf("can't get column index by name \"%v\": %v", columnName, err)
+		}
+		indices[i] = idx
+	}
+
+	return raw.FilterByIndices(indices)
+}
+
+// FilterByIndices returns RawCsv with provided header indices only
+func (raw *RawCsv) FilterByIndices(indices []int) (*RawCsv, error) {
+	rows := raw.Rows()
+	if rows == nil {
+		return nil, fmt.Errorf("RawCsv is empty.")
+	}
+
+	final := make([][]string, len(rows))
+	for i, r := range rows {
+		final[i] = make([]string, len(indices))
+		for j, c := range indices {
+			final[i][j] = r[c]
+		}
+	}
+
+	return RawCsvFromRows(final), nil
+}
+
+// GetSummary returns the summary of the RawCsv
+// It's basically grouping by the GroupBy field, and sum the SumBy columns of the RawCsv
+func (rawcsv *RawCsv) GetSummary(cfg *summary.SummaryConfig) (*summary.CsvSummary, error) {
+	if cfg.GroupBy == "" || cfg.SumBy == nil {
+		return nil, fmt.Errorf("can't produce CsvSummary: GroupBy and SumBy mustn't be empty")
+	}
+
+	filtered, err := rawcsv.FilterByNames(append([]string{cfg.GroupBy}, cfg.SumBy...))
+	if err != nil {
+		return nil, fmt.Errorf("can't filter by names: %v", err)
+	}
+
+	summ := make(map[string]([]float64))
+
+	for _, row := range filtered.Rows()[1:] {
+		if summ[row[0]] == nil {
+			floats, err := csvparseutils.StringSliceToFloat64(row[1:])
+			if err != nil {
+				return nil, fmt.Errorf("can't parse SumBy to slice of floats: %v", err)
+			}
+			summ[row[0]] = floats
+
+			continue
+		} else {
+			prevRow := summ[row[0]]
+			for j, v := range prevRow {
+				value, err := strconv.ParseFloat(row[j+1], 64)
+				if err != nil {
+					return nil, fmt.Errorf("can't parse '%v' to float64: %v", v, err)
+				}
+				prevRow[j] += value
+			}
+			summ[row[0]] = prevRow
+		}
+	}
+
+	return summary.New(filtered.Rows()[0], &summ), nil
+}
---
+++ pkg/csvparse/summary/summary.go
@@ -0,0 +1,43 @@
+package summary
+
+// SummaryConfig represents the configuration to
+type SummaryConfig struct {
+	// Header to group by
+	GroupBy string
+
+	// Header to sum the values
+	// Values should be convertable to float64
+	SumBy []string
+}
+
+// NewSummaryConfig returns a new SummaryConfig
+func NewSummaryConfig(groupBy string, sumBy []string) *SummaryConfig {
+	return &SummaryConfig{
+		GroupBy: groupBy,
+		SumBy:   sumBy,
+	}
+}
+
+// CsvSummary represents the summary
+type CsvSummary struct {
+	headers []string
+	inner   *map[string]([]float64)
+}
+
+// New returns the new CsvSummary
+func New(h []string, m *map[string]([]float64)) *CsvSummary {
+	return &CsvSummary{
+		headers: h,
+		inner:   m,
+	}
+}
+
+// Inner returns the key-value map of the CsvSummary
+func (summary CsvSummary) Inner() *map[string]([]float64) {
+	return summary.inner
+}
+
+// Inner returns the key-value map of the CsvSummary
+func (summary CsvSummary) Headers() []string {
+	return summary.headers
+}
