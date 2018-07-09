5bb284a1c174931b78e7e8f01e74efd45347ae99
Roman <churomann@gmail.com>
2018-07-09 14:06:35 +0300

    fix Test_Parse

--- pkg/cloud/gcpparser/parser_test.go
+++ pkg/cloud/gcpparser/parser_test.go
@@ -11,13 +11,21 @@ import (
 func Test_Parse(t *testing.T) {
 	f, _ := os.Open("./testdata/test1.csv")
 	r := bufio.NewReader(f)
-	records, _ := csv.NewReader(r).ReadAll()
+	records, err := csv.NewReader(r).ReadAll()
+	if err != nil {
+		t.Errorf("can not parse file from bucket: %v", err)
+	}
 
+	const minRecNum = 1
+	if len(records) < minRecNum {
+		t.Errorf("records number < 1")
+	}
 	// skip row with headers
-	res, err := Parse(records[1:])
+	res, err := Parse(records[minRecNum:])
 	if err != nil {
 		t.Errorf("err != nil: %v", err)
 	}
+	// compare record with data from "./testdata/test1.csv"
 	exp := "com.google.cloud/services/cloud-storage/StorageMultiRegionalUsGbsec"
 	if res[0].Item != exp {
 		t.Errorf("parse:\nExp: %v, Got: %v", exp, res[0].Item)
