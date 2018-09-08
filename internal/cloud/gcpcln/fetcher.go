a24f1a245f6cddb9370e1fe889286bcf75392e09
Roman <churomann@gmail.com>
2018-09-09 00:02:59 +0300

    Add fetcher

--- internal/cloud/gcpcln/fetcher.go
+++ internal/cloud/gcpcln/fetcher.go
@@ -1,43 +1,42 @@
 package gcpcln
 
 import (
-	"fmt"
 	"log"
 	"time"
 )
 
 // Fetch periodically fetches data into db from GCP
-func (c *Client) Fetch(bktName, prefix string, dt time.Duration) {
+func (c *Client) Fetch(bktName, prefix string, accountID int, dt time.Duration) {
 
 	// pull all csv objects from bucket
 	objs, err := c.CsvObjsList(bktName, prefix)
 	if err != nil {
 		log.Fatalf("in fetch c.CsvObjList: %v", err)
 	}
+	log.Println(objs)
 
 	// select last report from db with its creation time
 	lstFile, err := c.pgCln.GetLastFile()
 	if err != nil {
 		log.Printf("in fetch c.pgCln.SelLstGcpCsvObjectl: %v", err)
 	}
-	fmt.Println(lstFile)
+	log.Println(lstFile)
 
 	// filter CsvObjList, save only fresh objects
 	if lstFile != nil {
 		objs = objs.After(lstFile.Created)
 	}
 
-	// make Reports from object content
+	// // make Reports from object content
 	reps, err := c.MakeReports(objs)
 	if err != nil {
 		log.Fatalf("in fetch MakeReports: %v", err)
 	}
-	fmt.Println(reps)
+	log.Println(reps)
 
 	// write Reports into db
-	// c.pgCln.InsReps(reps)
-	// c.pgCln.InsObjs(pgcln.Objects(make(Objects, 10)))
-
-	// time.Sleep(dt * time.Hour)
-	// }
+	err = c.pgCln.AddReportsToAccount(reps, accountID)
+	if err != nil {
+		log.Fatalf("in fetch AddReportsToAccount: %v", err)
+	}
 }
--- internal/cloud/gcpcln/fetcher_test.go
+++ internal/cloud/gcpcln/fetcher_test.go
@@ -11,12 +11,15 @@ func Test_Client_Fetch(t *testing.T) {
 	ctx := context.Background()
 	cln, err := NewClient(ctx, makeConfig())
 	if err != nil {
-		t.Error("Failed to create client:", err)
+		t.Fatalf("Failed to create client: %v", err)
 	}
 
 	bktName := os.Getenv("APP_PROJECT_BUCKET")
 	prefix := "test"
 	dt := time.Second * 10
+	// NOTE: tester should add account with id 21 to db
+	accountID := 21
 
-	cln.Fetch(bktName, prefix, dt)
+	// run method fetch
+	cln.Fetch(bktName, prefix, accountID, dt)
 }
