c4d9a5a47c75727138703f57f2601c51f8b29c49
vvy <vemelin@ooyala.com>
2018-09-26 19:21:17 +0300

    bot messages to group

--- scripts/notify_tg.sh
+++ scripts/notify_tg.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-curl -X POST https://api.telegram.org/bot576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA/sendMessage -d chat_id=324044527 \
--d "text=${TRAVIS_BRANCH} (${TRAVIS_COMMIT_MESSAGE}) tests result: ${TRAVIS_TEST_RESULT}"
+curl -X POST https://api.telegram.org/bot576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA/sendMessage -d chat_id=-268977972 \
+-d "text=${TRAVIS_BRANCH} (${TRAVIS_COMMIT_MESSAGE}) tests return code: ${TRAVIS_TEST_RESULT}"
